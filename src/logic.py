4fb25c834d46dbf09cc11b69a6ba38180783e827
alexdelphi <alexander.edigarew@yandex.ru>
2018-11-06 11:53:39 +0300

    the points hopefully suitable for rendering

--- src/logic.py
+++ src/logic.py
@@ -19,7 +19,7 @@ class Model:
         self.radius = radius
         self.arc_length: float = radius / size  # расстояние одной дуги
 
-    def _get_points(self) -> typing.List[wx.Point2D]:
+    def arcs(self) -> typing.Dict[str, typing.List[typing.Tuple[wx.Point2D, wx.Point2D]]]:
         """
         Получаем точки ("сетку") для рисования.
         """
@@ -30,30 +30,37 @@ class Model:
         directions: typing.List[str] = ['up', 'right', 'left']
         direction_count: int = len(directions)
         # Углы поворота
-        angles: typing.Dict[str, float] = {name: i * full_angle / len(directions) for i, name in enumerate(directions)}
+        angles: typing.Dict[str, float] = {name: i * full_angle / direction_count for i, name in enumerate(directions)}
         # Векторы, соответствующие углам поворота.
         # На основании этих векторов потом вычисляется дистанция между точками
         # Эти векторы "математические", с длиной 1 по построению
         base_vectors: typing.List[typing.Tuple[str, wx.Point2D]] = [(name, wx.Point2D(
             math.cos(a), math.sin(a)
         )) for name, a in angles]
-        vectors: typing.List[wx.Point2D] = [angle for name, angle in base_vectors]
-        rotated_vectors = (vectors[(i + 1) % direction_count] for i in range(direction_count))
-        # Множество векторов на осях
-        points: typing.List[wx.Point2D] = [v * i for i in range (direction_count) for v in base_vectors]
-        # Все остальные векторы не на осях
-        points.extend([vec[0] * i + vec[1] * j
-                      for i in range(1, direction_count)
-                      for j in range(1, direction_count)
-                      for vec in itertools.zip_longest(vectors, rotated_vectors)])
+        points: typing.Dict[str, typing.List[typing.Tuple[wx.Point2D, wx.Point2D]]] = []
+        for displacement, name in enumerate(directions):
+            # Повернем векторы, заодно конвертируем List<Tuple<str, int>> -> Dictionary<str, int>
+            vectors: typing.Dict[str, wx.Point2D] = {k: v
+                                                     for i, _ in enumerate(base_vectors)
+                                                     for k, v in base_vectors[(i + displacement) % direction_count]}
+            points_below: typing.List[wx.Point2D] = [
+                row_disp * vectors['up'] + col_disp * v
+                # чтобы не повторять дважды последовательность для "правого" и "левого" векторов
+                for v, start in {vectors['right']: 0, vectors['left']: 1}.items()
+                for col_disp in range(start, self.size)
+                for row_disp in range(-col_disp, self.size)
+            ]
+            points[name] = [(x + vectors['up'], x) for x in points_below]
         return points
 
     @staticmethod
     def distance(x: wx.Point2D, y: wx.Point2D) -> float:
         return math.sqrt((y[1] - x[1]) ** 2 + (y[0] - x[0]) ** 2)
 
+    """
     def nearest_points(self) -> typing.List[typing.Tuple[wx.Point2D, wx.Point2D]]:
         points: typing.List[wx.Point2D] = self._get_points()
         return [
             (x, y) for x in points for y in points if abs(__class__.distance(x, y) - 1) <= 1e-3
         ]
+    """
